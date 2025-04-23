5c419d9f9b0ca628c2aba2b31369f4c959c9af40
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-04-23 15:43:58 +0530

    Update Jobs.jsx

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -11,7 +11,7 @@ const Jobs = () => {
 
   useEffect(() => {
     try {
-      axios.get("http://localhost:3000/api/v1/job/getall",{withCredentials: true})
+      axios.get("https://job-seeking-application-hirelynow-backend.onrender.com/api/v1/job/getall",{withCredentials: true})
       .then((res)=>{
         setJobs(res.data)
       })
