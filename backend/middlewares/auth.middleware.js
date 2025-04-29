f25b486ec635988fb02d86c84e9cb532bd226d4d
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-04-29 22:11:19 +0530

    Update auth.middleware.js

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import ErrorHandler from "./error.middleware.js"
 const isAuthorized = asyncHandler( async(req,res,next)=>{
     const {token} = req.cookies
     if(!token){
-        return next( new ErrorHandler("Unauthorized User!"))
+        return next( new ErrorHandler("Unauthorized User!", 401))
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
     req.user = await userModel.findById(decoded.id)
